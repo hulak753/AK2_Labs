14fd6d9268dad60e5a4be18c0c6574a29b9720be
Hulak Ivan <hulak_ivan753@ukr.net>
2021-10-15 03:45:54 +0300

    adding own changes

--- calculator.cpp
+++ calculator.cpp
@@ -14,3 +14,22 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+
+int main() {
+    int n;
+    long double factorial = 1.0;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    if (n < 0)
+        cout << "Error! Factorial of a negative number doesn't exist.";
+    else {
+        for(int i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        cout << "Factorial of " << n << " = " << factorial;    
+    }
+
+    return 0;
+}
